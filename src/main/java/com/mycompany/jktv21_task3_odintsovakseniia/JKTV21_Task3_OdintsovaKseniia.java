e67afb06434d28cc374dff796951354054caebcd
KseniiaOdintsovaIVKHK <kseniia.odintsova@ivkhk.ee>
2022-09-26 18:40:36 +0300

    Task3

---
+++ src/main/java/com/mycompany/jktv21_task3_odintsovakseniia/JKTV21_Task3_OdintsovaKseniia.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.jktv21_task3_odintsovakseniia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class JKTV21_Task3_OdintsovaKseniia {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello User");
+        System.out.print("Please, put Your First name : ");
+        String name = scanner.next();
+        
+        System.out.print("Please, put Your Second name : ");
+        String family = scanner.next();
+        
+        System.out.print("Please, put Your personal number name : ");
+        int number = scanner.nextInt();
+        
+       
+    
+    
+         
+    }
+        
+    }
+
